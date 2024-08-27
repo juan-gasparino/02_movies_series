d02b537a06fa98d1e905b774de8b940a6239d027
Juan Gasparino <juan.gasparino@gmail.com>
2024-08-27 23:25:24 +0200

    update tables db

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    image_url = models.URLField(default='https://via.placeholder.com/150')
+    description = models.TextField()
+    year = models.IntegerField()
+    likes = models.IntegerField()
+    followed_by = models.ManyToManyField(User, related_name='followed_movies')
+    release_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- series/models.py
+++ series/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Serie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    image_url = models.URLField(default='https://via.placeholder.com/150')
+    description = models.TextField()
+    seasons = models.IntegerField()
+    year = models.IntegerField()
+    likes = models.IntegerField()
+    followed_by = models.ManyToManyField(User, related_name='followed_series')
+    first_episode = models.DateTimeField(auto_now_add=True)
+    last_episode = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
